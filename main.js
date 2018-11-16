b27a37e7acb9fa48edd8c3d85dff7800cb561640
Aliyah Northington <alierin@Aliyahs-MBP.lan>
2018-11-16 11:39:50 -0600

    Add click event to button element
    The click event will trigger the slideToggle function.
    Also added forgotten () to my function.

--- main.js
+++ main.js
@@ -2,9 +2,10 @@
 var section = document.querySelector('section.hide');
 var button = document.querySelector('button');
 
-function slideToggle{
+function slideToggle(){
 	section.classList.toggle('hide');
 };
 
+button.addEventListener('click', slideToggle);
 
 //jQuery functionality will go here. Make sure to iclude the document.ready function.
