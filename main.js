9272276645088b0cb3bfeb954b1a25b7d6f63451
Aliyah Northington <alierin@Aliyahs-MBP.lan>
2018-11-16 11:48:06 -0600

    Select the button with jQuery and add click event

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-//JavaScript Functionalit Part 1 Goes Here. Comment it all out once finsihed.
-var section = document.querySelector('section.hide');
-var button = document.querySelector('button');
+//JavaScript Functionality Part 1 Goes Here. Comment it all out once finished.
+//var section = document.querySelector('section.hide');
+//var button = document.querySelector('button');
+//
+//function slideToggle(){
+//	section.classList.toggle('hide');
+//};
+//
+//button.addEventListener('click', slideToggle);
 
-function slideToggle(){
-	section.classList.toggle('hide');
-};
-
-button.addEventListener('click', slideToggle);
-
-//jQuery functionality will go here. Make sure to iclude the document.ready function.
+//jQuery functionality will go here. Make sure to include the document.ready function.
+$(document).ready(function(){
+	$('button').click(function(){
+		
+	})
+});
